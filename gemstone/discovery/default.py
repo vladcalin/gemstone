e54bcfa2a283641d1cad830003d9498788e6f638
Calin Vlad <vlad.s.calin@gmail.com>
2017-03-10 21:05:09 +0200

    added basic default discovery strategy

---
+++ gemstone/discovery/default.py
@@ -0,0 +1,37 @@
+import random
+import string
+
+from tornado.gen import coroutine
+from tornado.ioloop import IOLoop
+from tornado.httpclient import HTTPClient, HTTPRequest
+import simplejson as json
+
+from gemstone.discovery.base import BaseDiscoveryStrategy
+
+
+class DefaultDiscoveryStrategy(BaseDiscoveryStrategy):
+    def __init__(self, registry_location):
+        self.registry = registry_location
+
+    def make_jsonrpc_call(self, url, method, params):
+        client = HTTPClient()
+        body = json.dumps({
+            "jsonrpc": "2.0",
+            "method": method,
+            "params": params,
+            "id": "".join([random.choice(string.ascii_letters) for _ in range(10)])
+        })
+
+        request = HTTPRequest(url, method="POST", headers={"content-type": "application/json"},
+                              body=body)
+        result = client.fetch(request)
+        return result
+
+    def ping(self, name, location, **kwargs):
+        self.make_jsonrpc_call(self.registry, "ping",
+                               {"name": name, "url": location})
+
+    def locate(self, name):
+        response = self.make_jsonrpc_call(self.registry, "locate_service", {"name": name})
+        resp_as_dict = json.loads(response.body.decode())
+        return resp_as_dict["result"]
